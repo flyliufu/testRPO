5ed8ad4edd9d74417d7314bbf3704ea5dd11c04f
fangchao <okay_fc@sina.com>
2016-03-30 23:08:02 +0800

    测试gulp搭建本地服务器

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+
+gulp.task('serve', function() {
+    $.connect.server({
+        livereload: true
+    });
+});
+gulp.task('less',function(){
+	gulp.src('app/styles/**/*.less')
+	.pipe($.less())
+	.pipe(gulp.dest('app/styles'))
+	.pipe($.connect.reload());
+});
+gulp.task('watch',function(){
+	gulp.watch('app/styles/**/*.less',['less']);
+});
+
+gulp.task('default',['less','serve','watch']);
 // var less = require('gulp-less');
 
 // gulp.task('less',function(){
@@ -13,21 +30,21 @@ var path = {
 };
 
 
-var connect = require('gulp-connect');
-gulp.task('watch', function() {
-    gulp.watch(['./www/*.html'], ['html']);
-});
+// var connect = require('gulp-connect');
+// gulp.task('watch', function() {
+//     gulp.watch(['app/views/*.html'], ['html']);
+// });
 
-gulp.task('connect', function() {
-    connect.server({
-        root: 'www',
-        livereload: true
-    });
-});
+// gulp.task('connect', function() {
+//     connect.server({
+//         root: 'www',
+//         livereload: true
+//     });
+// });
 
-gulp.task('html', function() {
-    gulp.src('./www/*.html')
-        .pipe(connect.reload());
-});
+// gulp.task('html', function() {
+//     gulp.src('./www/*.html')
+//         .pipe(connect.reload());
+// });
 
-gulp.task('default', ['connect', 'watch']);
+// gulp.task('default', ['connect', 'watch']);
