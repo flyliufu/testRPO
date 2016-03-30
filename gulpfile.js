06943eeb04e2c6601bb872909fc47512ebb5236e
gitdust <okay_fc@sina.com>
2016-03-30 17:23:12 +0800

    测试gulp插件使用

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var $ = require('gulp-load-plugins')();
+// var less = require('gulp-less');
 
-gulp.task('less',function(){
-	gulp.src('styles/**/*.less')
-	.pipe(less())
-	.pipe(gulp.dest('styles'));
+// gulp.task('less',function(){
+// 	gulp.src('styles/**/*.less')
+// 	.pipe(less())
+// 	.pipe(gulp.dest('styles'));
+// });
+
+
+
+var connect = require('gulp-connect');
+gulp.task('watch',function(){
+	gulp.watch(['./www/*.html'],['html']);
 });
 
-gulp.task('default',['less']);
+gulp.task('connect',function(){
+	connect.server({
+		root: 'www',
+		livereload: true
+	});
+});
+
+gulp.task('html',function(){
+	gulp.src('./www/*.html')
+	.pipe(connect.reload());
+});
+
+gulp.task('default',['connect','watch']);
