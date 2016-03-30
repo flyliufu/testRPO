bb57384341b665be7078c5a1b7283e7e56471e51
gitdust <okay_fc@sina.com>
2016-03-30 17:37:42 +0800

    gulpfile新增路径全局变量

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,27 @@ var $ = require('gulp-load-plugins')();
 // 	.pipe(less())
 // 	.pipe(gulp.dest('styles'));
 // });
-
+var path = {
+    app: 'app',
+    dist: 'dist'
+};
 
 
 var connect = require('gulp-connect');
-gulp.task('watch',function(){
-	gulp.watch(['./www/*.html'],['html']);
+gulp.task('watch', function() {
+    gulp.watch(['./www/*.html'], ['html']);
 });
 
-gulp.task('connect',function(){
-	connect.server({
-		root: 'www',
-		livereload: true
-	});
+gulp.task('connect', function() {
+    connect.server({
+        root: 'www',
+        livereload: true
+    });
 });
 
-gulp.task('html',function(){
-	gulp.src('./www/*.html')
-	.pipe(connect.reload());
+gulp.task('html', function() {
+    gulp.src('./www/*.html')
+        .pipe(connect.reload());
 });
 
-gulp.task('default',['connect','watch']);
+gulp.task('default', ['connect', 'watch']);
